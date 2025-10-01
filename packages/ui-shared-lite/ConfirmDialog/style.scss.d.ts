149e639915e779b565873104971560b74f6567ff
Utsav Poudyal <utsavpoudyal1120@gmail.com>
2025-10-01 21:05:08 +0545

    feat: style scss change when building ui shared

--- packages/ui-shared-lite/ConfirmDialog/style.scss.d.ts
+++ packages/ui-shared-lite/ConfirmDialog/style.scss.d.ts
@@ -4,5 +4,5 @@ interface CssExports {
   'ConfirmDialogContainer': string;
   'icon': string;
 }
-declare var cssExports: CssExports;
-export = cssExports;
+export const cssExports: CssExports;
+export default cssExports;
